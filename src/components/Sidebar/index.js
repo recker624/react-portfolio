dea9befccb7293652acaa960fe0af7fa5379de1d
recker624 <52703428+recker624@users.noreply.github.com>
2023-07-05 13:45:27 +0530

    Implemented home page
    main home page implementation but svg animation is still left.

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -14,13 +14,13 @@ const Sidebar = () => {
         <img className="sub-logo" src={LogoSubtitle} alt="Sahil" />
       </Link>
       <nav>
-        <NavLink exact="true" activeClassName="active" to="/">
+        <NavLink exact="true" activeclassname="active" to="/">
           <FontAwesomeIcon icon={faHome} color='#4d4d4e'/>
         </NavLink>
-        <NavLink exact="true" activeClassName="active" className="about-link" to="/about">
+        <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
           <FontAwesomeIcon icon={faUser} color='#4d4d4e'/>
         </NavLink>
-        <NavLink exact="true" activeClassName="active" className="contact-link" to="/contact">
+        <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
           <FontAwesomeIcon icon={faEnvelope} color='#4d4d4e'/>
         </NavLink>
       </nav>
