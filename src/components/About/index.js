e1ffff98369afd2d1bf96cf61697b83af5648a09
recker624 <52703428+recker624@users.noreply.github.com>
2023-07-06 08:29:09 +0530

    Added page loader animation

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -3,6 +3,7 @@ import AnimatedLetters from "../AnimatedLetters";
 import "./index.scss"
 import { useState, useEffect } from "react";
 import { faBootstrap, faCss3, faGitAlt, faHtml5, faJsSquare, faReact } from "@fortawesome/free-brands-svg-icons";
+import Loader from "react-loaders";
 
 const About = () => {
 
@@ -17,49 +18,52 @@ const About = () => {
   }, [])
 
   return (
-    <div className="container about-page">
-      <div className="text-zone">
-        <h1>
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
-            idx={15}
-          />
-        </h1>
-        <p>
-          As a highly ambitious front-end developer, I am actively seeking a position within
-          a well-established IT company that offers an exciting opportunity to
-          collaborate on cutting-edge technologies and engage in challenging projects with diverse scopes.
-        </p>
-        <p>
-          I'm quite confidant and curious , and am dedicated to honing my skills as a web developer. With each design problem I encounter,
-          I embrace the opportunity for growth and improvement, improving myself one problem at a time.
-        </p>
-      </div>
+    <>
+      <div className="container about-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
+              idx={15}
+            />
+          </h1>
+          <p>
+            As a highly ambitious front-end developer, I am actively seeking a position within
+            a well-established IT company that offers an exciting opportunity to
+            collaborate on cutting-edge technologies and engage in challenging projects with diverse scopes.
+          </p>
+          <p>
+            I'm quite confidant and curious , and am dedicated to honing my skills as a web developer. With each design problem I encounter,
+            I embrace the opportunity for growth and improvement, improving myself one problem at a time.
+          </p>
+        </div>
 
-      <div className='stage-cube-count'>
-        <div className="cubespinner">
-          <div className="face1">
-            <FontAwesomeIcon icon={faReact} color="#5ed4f4" />
-          </div>
-          <div className="face2">
-            <FontAwesomeIcon icon={faHtml5} color="#f06529" />
-          </div>
-          <div className="face3">
-            <FontAwesomeIcon icon={faCss3} color="#28a4d9" />
-          </div>
-          <div className="face4">
-            <FontAwesomeIcon icon={faJsSquare} color="#efd81d" />
-          </div>
-          <div className="face5">
-            <FontAwesomeIcon icon={faGitAlt} color="#efd81d" />
-          </div>
-          <div className="face6">
-            <FontAwesomeIcon icon={faBootstrap} color="#6f42c1" />
+        <div className='stage-cube-count'>
+          <div className="cubespinner">
+            <div className="face1">
+              <FontAwesomeIcon icon={faReact} color="#5ed4f4" />
+            </div>
+            <div className="face2">
+              <FontAwesomeIcon icon={faHtml5} color="#f06529" />
+            </div>
+            <div className="face3">
+              <FontAwesomeIcon icon={faCss3} color="#28a4d9" />
+            </div>
+            <div className="face4">
+              <FontAwesomeIcon icon={faJsSquare} color="#efd81d" />
+            </div>
+            <div className="face5">
+              <FontAwesomeIcon icon={faGitAlt} color="#efd81d" />
+            </div>
+            <div className="face6">
+              <FontAwesomeIcon icon={faBootstrap} color="#6f42c1" />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      <Loader type="ball-clip-rotate-pulse"/>
+    </>
   );
 }
 
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -5,6 +5,7 @@ import Logo from './Logo';
 import { Link } from 'react-router-dom';
 import React, { useEffect, useState }  from 'react';
 import AnimatedLetters from '../AnimatedLetters';
+import Loader from 'react-loaders';
 
 
 const Home = () => {
@@ -21,30 +22,33 @@ const Home = () => {
   }, [])
 
   return (
-    <div className="container home-page">
-      <div className="text-zone">
-        <h1>
-          <span className={letterClass}>H</span>
-          <span className={`${letterClass} _12`}>i</span>
-          <br />
-          <span className={`${letterClass} _13`}>I</span>
-          <span className={`${letterClass} _14`}>'m</span>
-          <img src={LogoTitle} alt="developer" />
-          <AnimatedLetters letterClass={letterClass}
-            strArray={nameArray}
-            idx={15}
-          />
-          <br />
-          <AnimatedLetters letterClass={letterClass}
-            strArray={jobArray}
-            idx={19}
-          />
-        </h1>
-        <h2>Frontend Developer</h2>
-        <Link to="/contact" className="flat-button">CONTACT ME</Link>
-      </div>    
-      <Logo/>
-    </div>
+    <>
+      <div className="container home-page">
+        <div className="text-zone">
+          <h1>
+            <span className={letterClass}>H</span>
+            <span className={`${letterClass} _12`}>i</span>
+            <br />
+            <span className={`${letterClass} _13`}>I</span>
+            <span className={`${letterClass} _14`}>'m</span>
+            <img src={LogoTitle} alt="developer" />
+            <AnimatedLetters letterClass={letterClass}
+              strArray={nameArray}
+              idx={15}
+            />
+            <br />
+            <AnimatedLetters letterClass={letterClass}
+              strArray={jobArray}
+              idx={19}
+            />
+          </h1>
+          <h2>Frontend Developer</h2>
+          <Link to="/contact" className="flat-button">CONTACT ME</Link>
+        </div>    
+        <Logo/>
+      </div>
+      <Loader type="ball-clip-rotate-pulse"/>
+    </>
   );
 }
 
